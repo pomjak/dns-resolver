bb85052ed780cb3e99318d7e043d97bfdf4323f0
pomjak <pomjak71@gmail.com>
2023-09-29 21:01:35 +0200

    added getopt

--- src/dns.cc
+++ src/dns.cc
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
 #include <getopt.h>
 
-int main()
+int get_args(int argc, char **argv)
 {
+    int opt;
+    while (opt = getopt(argc, argv, "rx6spa;"))
+    {
+        switch(opt)
+        {
+            
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+
     return 0;
 }
