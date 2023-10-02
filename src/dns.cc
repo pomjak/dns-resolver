bfeb16e95235926c9f780d5a9a8ea0436e603a79
pomjak <pomjak71@gmail.com>
2023-10-02 12:35:05 +0200

    get opt ok

--- src/dns.cc
+++ src/dns.cc
@@ -3,36 +3,53 @@
 #include <unistd.h>
 #include <getopt.h>
 
-int get_args(int argc, char **argv)
+void get_args(int argc, char **argv)
 {
     int opt;
-    while (opt = getopt(argc, argv, "rx6s:p") != -1)
+    std::string server;
+    std::string port;
+    std::string address;
+    while ((opt = getopt(argc, argv, "rx6s:p:")) != -1)
     {
         switch(opt)
         {
           case 'r':
+            std::cout << "reverse" << std::endl;
           break;
 
           case 'x':
+            std::cout << "turncated" << std::endl;
           break;
 
           case '6':
+            std::cout << "ip6" << std::endl;
           break;
 
           case 's':
+            server = optarg;
+            std::cout << "server: " << server << std::endl;
           break;
 
           case 'p':
+            port = optarg;
+            std::cout << "port" << port <<std::endl;
           break;
-          
-          deafult:
+
+          default:
+            std::cout << "usage" << std::endl;
           break;
         }
+    }
+    if (optind < argc) {
+        address = argv[optind];
+        std::cout << address << std::endl;
+    } else {
+        std::cerr << "Address is a required parameter." << std::endl;
     }
 }
 
 int main(int argc, char **argv)
 {
-
+    get_args(argc,argv);
     return 0;
 }
