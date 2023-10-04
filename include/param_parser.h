db1b32f07c638a1a155968ef3fb539d1df671600
pomjak <pomjak71@gmail.com>
2023-10-05 00:03:47 +0200

    edit flags to struct

--- include/param_parser.h
+++ include/param_parser.h
@@ -9,50 +9,53 @@
 class param_parser
 {
 private:
+    struct flags{
+        bool recursion;
+        bool reverse;
+        bool ipv6;
+        std::string server;
+        int port;
+        std::string address;
+    };
 
-    bool recursion;
-    bool reverse;
-    bool ipv6;
-    std::string server;
-    int port;
-    std::string address;
+    flags flag;
     
-    void set_recursion(bool r) { this->recursion = r; }
+    void set_recursion(bool r) { this->flag.recursion = r; }
 
-    void set_reverse(bool x) { this->reverse = x; }
+    void set_reverse(bool x) { this->flag.reverse = x; }
 
-    void set_ipv6(bool ip) { this->ipv6 = ip; }
+    void set_ipv6(bool ip) { this->flag.ipv6 = ip; }
 
-    void set_server(std::string s) { this->server = s; }
+    void set_server(std::string s) { this->flag.server = s; }
 
-    void set_port(int p) { this->port = p; }
+    void set_port(int p) { this->flag.port = p; }
 
-    void set_address(std::string a) { this->address = a; }
+    void set_address(std::string a) { this->flag.address = a; }
 
 public:
     param_parser()
     {
-        this->recursion = false;
-        this->reverse = false;
-        this->ipv6 = false;
-        this->server = "";
-        this->port = -1;
-        this->address = "";
+        this->flag.recursion = false;
+        this->flag.reverse = false;
+        this->flag.ipv6 = false;
+        this->flag.server = "";
+        this->flag.port = -1;
+        this->flag.address = "";
     }
 
     void process_params(int argc, char **argv);
 
-    bool get_recursion(void) { return this->recursion; }
+    bool get_recursion(void) { return this->flag.recursion; }
 
-    bool get_reverse(void) { return this->reverse; }
+    bool get_reverse(void) { return this->flag.reverse; }
 
-    bool get_ipv6(void) { return this->ipv6; }
+    bool get_ipv6(void) { return this->flag.ipv6; }
 
-    std::string get_server(void) { return this->server; }
+    std::string get_server(void) { return this->flag.server; }
 
-    int get_port(void) { return this->port; }
+    int get_port(void) { return this->flag.port; }
 
-    std::string get_address(void) { return this->address; }
+    std::string get_address(void) { return this->flag.address; }
 };
 
 #endif
