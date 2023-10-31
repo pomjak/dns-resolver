0ed6d2fff45caae8426209084f817f5dc6d05069
pomjak <pomjak71@gmail.com>
2023-10-31 23:24:28 +0100

    python magic aint happenin

--- test/test.py
+++ test/test.py
@@ -1,40 +1,31 @@
 import subprocess
 
-DIG = "dig @147.229.8.12 +noall +answer"
-PROG = "test -s 147.229.8.12 "
-
 RED = '\033[0;31m'
 GREEN = '\033[0;32m'
 YELLOW = '\033[0;33m'
 RESET = '\033[0m'
 
-def run_command(command):
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    return result.stdout
+def run_command(*command):
+        popen = subprocess.Popen(command, stdout=subprocess.PIPE)
+        popen.wait()
+        return popen.stdout.read()
+
 
 def main():
     with open('domains', 'r') as domain_file: 
         for domain in domain_file:
             domain = domain.strip()
 
-            dig_output = run_command(f"{DIG} {domain}")
-            prog_output = run_command(f"{PROG} {domain}")
+            dig_output = run_command("dig", "@147.229.8.12", "+noall", "+answer", "{domain}")
+            prog_output = run_command("test", "-s", "147.229.8.12", "{domain}")
+            print(prog_output)
+            print(dig_output)
 
             no_dig = len(dig_output.splitlines())
 
-            parsed_dns = None
-            lines = prog_output.splitlines()
-            last_complete = ""
-            for line in lines:
-                if "Authority" in line:
-                    break
-                last_complete = line
-
-            parsed_dns = last_complete
-
-            no_dns = len(parsed_dns.splitlines())
 
             for i in range(1, no_dig + 1):
+
                 dig_line = dig_output.splitlines()[i - 1]
                 dig_name, dig_ttl, dig_class, dig_type, dig_data = dig_line.split()
                 dig_name = dig_name.strip()
