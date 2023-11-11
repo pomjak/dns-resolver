9476775dba4fb4e99fd52b6e9da686b13a822ac6
pomjak <pomjak71@gmail.com>
2023-11-11 16:13:47 +0100

    test: edit domains; edit main test for more testcases

--- test/test.py
+++ test/test.py
@@ -5,6 +5,13 @@ GREEN = '\033[0;32m'
 YELLOW = '\033[0;33m'
 RESET = '\033[0m'
 
+commands = [
+    {'desc':'A record','dig':'dig -r @8.8.8.8 +noall +answer A ', 'dns':'./test -r -s 8.8.8.8 '},
+    {'desc':'AAAA record','dig':'dig -r @8.8.8.8 +noall +answer AAAA ', 'dns':'./test -r -s 8.8.8.8 -6 '},
+    {'desc':'reverse A record','dig':'dig -r @8.8.8.8 +noall +answer -x ', 'dns':'./test -r -s 8.8.8.8 -x '},
+    {'desc':'reverse AAAA record','dig':'dig -r @8.8.8.8 +noall +answer -x AAAA ', 'dns':'./test -r -s 8.8.8.8 -x -6 '},
+    ]
+
 def run_command(cmd):   
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
@@ -29,12 +36,12 @@ def cut_out_answer(data):
 
 def main():
     with open('domains', 'r') as domain_file: 
-        for domain in domain_file:\
+        for domain in domain_file:
             # domain = "pinterest.com"
             domain = domain.strip()
 
-            dig_output, dig_stderr = run_command("dig @dns.google +noall +answer " + domain)
-            dns_output,dns_stderr = run_command("./test -s dns.google " + domain)
+            dig_output, dig_stderr = run_command(commands[1]['dig'] + domain)
+            dns_output,dns_stderr = run_command(commands[1]['dns'] + domain)
             
             dig_output=dig_output[:-1].decode("utf-8")
             dns_output=cut_out_answer(dns_output.decode("utf-8"))
@@ -52,7 +59,7 @@ def main():
                     new_record = {'name': name, 'class': rclass, 'type' : rtype, 'ttl' : ttl,'data' : data}
                     dig.append(new_record)
                 except ValueError:
-                    print(f"Error parsing dig line: {dig_line}")
+                    print(f"Error parsing dig line: {dig_line} with {domain}")
                     exit(1)
                 try:
                     name, rtype, rclass, ttl, data = dns_line.split(",")
@@ -65,49 +72,37 @@ def main():
                     new_record = {'name': name, 'class': rclass, 'type' : rtype, 'ttl' : ttl,'data' : data}
                     dns.append(new_record)
                 except ValueError:
-                    print(f"Error parsing dns line: {dns_line}")
+                    print(f"Error parsing dns line: {dns_line} with {domain}")
                     exit(1)
                 
                 
             dig.sort(key=lambda x: x['data'])
             dns.sort(key=lambda x: x['data'])
-            # print(dig)
-            # print(dns)
             
             for dig_record,dns_record in zip(dig,dns):
                 
                 if dig_record['name'] != dns_record['name']:
-                    print(dig_record['name'])
-                    print(dns_record['name'])
                     print(f"Test {RED}FAILED{RESET} [name of {domain}]")
                     print(f"Expected \"{dig_record['name']}\", got \"{dns_record['name']}\"")
                     exit(1)
+
                 elif dig_record['class'] != dns_record['class']:
-                    print(dig_record)
-                    print(dns_record)
                     print(f"Test {RED}FAILED{RESET} [class of {domain}]")
                     print(f"Expected \"{dig_record['class']}\", got \"{dns_record['class']}\"")
                     exit(1)
+
                 elif dig_record['type'] != dns_record['type']:
-                    print(dig_record)
-                    print(dns_record)
                     print(f"Test {RED}FAILED{RESET} [type of {domain}]")
                     print(f"Expected \"{dig_record['type']}\", got \"{dns_record['type']}\"")
                     exit(1)
+
                 elif dig_record['data'] != dns_record['data']:
-                    print(dig_record)
-                    print(dns_record)
                     print(f"Test {RED}FAILED{RESET} [addr of {domain}]")
                     print(f"Expected \"{dig_record['data']}\", got \"{dns_record['data']}\"")
                     exit(1)
-                elif dig_record['ttl'] != dns_record['ttl']:
-                    print(dig_record)
-                    print(dns_record)
-                    print(f"Test {RED}FAILED{RESET} [addr of {domain}]")
-                    print(f"Expected \"{dig_record['ttl']}\", got \"{dns_record['ttl']}\"")
-                    exit(1)
+
                 else:
-                    print(f"Test {GREEN}SUCCEEDED{RESET} [ {domain} ]")
+                    print(f"Test {GREEN}OK{RESET} [ {domain} ]")
 
 if __name__ == "__main__":
     main()
