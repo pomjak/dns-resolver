2dd713f69a9bc44f1633d698c62214cc2c871f4c
pomjak <pomjak71@gmail.com>
2023-11-11 16:54:48 +0100

    test: refactor

--- test/test.py
+++ test/test.py
@@ -6,10 +6,10 @@ YELLOW = '\033[0;33m'
 RESET = '\033[0m'
 
 commands = [
-    {'desc':'A record','dig':'dig -r @8.8.8.8 +noall +answer A ', 'dns':'./test -r -s 8.8.8.8 '},
-    {'desc':'AAAA record','dig':'dig -r @8.8.8.8 +noall +answer AAAA ', 'dns':'./test -r -s 8.8.8.8 -6 '},
-    {'desc':'reverse A record','dig':'dig -r @8.8.8.8 +noall +answer -x ', 'dns':'./test -r -s 8.8.8.8 -x '},
-    {'desc':'reverse AAAA record','dig':'dig -r @8.8.8.8 +noall +answer -x AAAA ', 'dns':'./test -r -s 8.8.8.8 -x -6 '},
+    {'desc':'A record',             'file':'domains',   'dig':'dig -r @8.8.8.8 +noall +answer A ',          'dns':'./test -r -s 8.8.8.8 '       },
+    {'desc':'AAAA record',          'file':'domains',   'dig':'dig -r @8.8.8.8 +noall +answer AAAA ',       'dns':'./test -r -s 8.8.8.8 -6 '    },
+    {'desc':'reverse A record',     'file':'ips',       'dig':'dig -r @8.8.8.8 +noall +answer -x A ',       'dns':'./test -r -s 8.8.8.8 -x '    },
+    {'desc':'reverse AAAA record',  'file':'ips6',      'dig':'dig -r @8.8.8.8 +noall +answer -x AAAA ',    'dns':'./test -r -s 8.8.8.8 -x -6 ' },
     ]
 
 def run_command(cmd):   
@@ -35,74 +35,79 @@ def cut_out_answer(data):
     return '\n'.join(result_lines[:-1])
 
 def main():
-    with open('domains', 'r') as domain_file: 
-        for domain in domain_file:
-            # domain = "pinterest.com"
-            domain = domain.strip()
-
-            dig_output, dig_stderr = run_command(commands[1]['dig'] + domain)
-            dns_output,dns_stderr = run_command(commands[1]['dns'] + domain)
+    for test_case in commands:
+        with open(test_case['file'], 'r') as domain_file: 
             
-            dig_output=dig_output[:-1].decode("utf-8")
-            dns_output=cut_out_answer(dns_output.decode("utf-8"))
-            dig = []
-            dns = []
-
-            for dig_line,dns_line in zip(dig_output.split('\n'),dns_output.split('\n')):
-                try:
-                    name, ttl, rclass, rtype, data = dig_line.split()
-                    name = name.strip()
-                    rclass = rclass.strip()
-                    rtype = rtype.strip()
-                    data = data.strip()
-
-                    new_record = {'name': name, 'class': rclass, 'type' : rtype, 'ttl' : ttl,'data' : data}
-                    dig.append(new_record)
-                except ValueError:
-                    print(f"Error parsing dig line: {dig_line} with {domain}")
-                    exit(1)
-                try:
-                    name, rtype, rclass, ttl, data = dns_line.split(",")
-                    name = name.strip()
-                    rtype = rtype.strip()
-                    rclass = rclass.strip()
-                    ttl = ttl.strip()
-                    data = data.strip()
-
-                    new_record = {'name': name, 'class': rclass, 'type' : rtype, 'ttl' : ttl,'data' : data}
-                    dns.append(new_record)
-                except ValueError:
-                    print(f"Error parsing dns line: {dns_line} with {domain}")
-                    exit(1)
-                
-                
-            dig.sort(key=lambda x: x['data'])
-            dns.sort(key=lambda x: x['data'])
+            print("__________ " + test_case['desc'] + " Test__________")
             
-            for dig_record,dns_record in zip(dig,dns):
+            for domain in domain_file:
                 
-                if dig_record['name'] != dns_record['name']:
-                    print(f"Test {RED}FAILED{RESET} [name of {domain}]")
-                    print(f"Expected \"{dig_record['name']}\", got \"{dns_record['name']}\"")
-                    exit(1)
+                domain = domain.strip()
 
-                elif dig_record['class'] != dns_record['class']:
-                    print(f"Test {RED}FAILED{RESET} [class of {domain}]")
-                    print(f"Expected \"{dig_record['class']}\", got \"{dns_record['class']}\"")
-                    exit(1)
+                dig_output, dig_stderr = run_command(test_case['dig'] + domain)
+                dns_output,dns_stderr = run_command(test_case['dns'] + domain)
+                
+                dig_output=dig_output[:-1].decode("utf-8")
+                dns_output=cut_out_answer(dns_output.decode("utf-8"))
+                
+                dig = []
+                dns = []
 
-                elif dig_record['type'] != dns_record['type']:
-                    print(f"Test {RED}FAILED{RESET} [type of {domain}]")
-                    print(f"Expected \"{dig_record['type']}\", got \"{dns_record['type']}\"")
-                    exit(1)
+                for dig_line,dns_line in zip(dig_output.split('\n'),dns_output.split('\n')):
+                    try:
+                        name, ttl, rclass, rtype, data = dig_line.split()
+                        name = name.strip()
+                        rclass = rclass.strip()
+                        rtype = rtype.strip()
+                        data = data.strip()
 
-                elif dig_record['data'] != dns_record['data']:
-                    print(f"Test {RED}FAILED{RESET} [addr of {domain}]")
-                    print(f"Expected \"{dig_record['data']}\", got \"{dns_record['data']}\"")
-                    exit(1)
+                        new_record = {'name': name, 'class': rclass, 'type' : rtype, 'ttl' : ttl,'data' : data}
+                        dig.append(new_record)
+                    except ValueError:
+                        print(f"Error parsing dig line: {dig_line} with {domain}")
+                        exit(1)
+                        
+                    try:
+                        name, rtype, rclass, ttl, data = dns_line.split(",")
+                        name = name.strip()
+                        rtype = rtype.strip()
+                        rclass = rclass.strip()
+                        ttl = ttl.strip()
+                        data = data.strip()
 
-                else:
-                    print(f"Test {GREEN}OK{RESET} [ {domain} ]")
+                        new_record = {'name': name, 'class': rclass, 'type' : rtype, 'ttl' : ttl,'data' : data}
+                        dns.append(new_record)
+                    except ValueError:
+                        print(f"Error parsing dns line: {dns_line} with {domain}")
+                        exit(1)
+                    
+                dig.sort(key=lambda x: x['data'])
+                dns.sort(key=lambda x: x['data'])
+                
+                for dig_record,dns_record in zip(dig,dns):
+                    
+                    if dig_record['name'] != dns_record['name']:
+                        print(f"Test {RED}FAILED{RESET} [name of {domain}]")
+                        print(f"Expected \"{dig_record['name']}\", got \"{dns_record['name']}\"")
+                        exit(1)
+
+                    elif dig_record['class'] != dns_record['class']:
+                        print(f"Test {RED}FAILED{RESET} [class of {domain}]")
+                        print(f"Expected \"{dig_record['class']}\", got \"{dns_record['class']}\"")
+                        exit(1)
+
+                    elif dig_record['type'] != dns_record['type']:
+                        print(f"Test {RED}FAILED{RESET} [type of {domain}]")
+                        print(f"Expected \"{dig_record['type']}\", got \"{dns_record['type']}\"")
+                        exit(1)
+
+                    elif dig_record['data'] != dns_record['data']:
+                        print(f"Test {RED}FAILED{RESET} [addr of {domain}]")
+                        print(f"Expected \"{dig_record['data']}\", got \"{dns_record['data']}\"")
+                        exit(1)
+
+                    else:
+                        print(f"Test {GREEN}OK{RESET} [ {domain} ]")
 
 if __name__ == "__main__":
     main()
